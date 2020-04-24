f603019b0be8cfab9ff34cadbd9aa5f217395647
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-04-24 14:47:28 -0400

    Update variables on HCL flag changes

--- operator/pkg/controller/workspace/workspace_controller.go
+++ operator/pkg/controller/workspace/workspace_controller.go
@@ -234,11 +234,6 @@ func (r *ReconcileWorkspace) Reconcile(request reconcile.Request) (reconcile.Res
 	if updatedTerraform || updatedVariables || instance.Status.RunID == "" {
 		r.reqLogger.Info("Starting run because template changed", "Organization", organization, "Name", workspace, "Namespace", request.Namespace)
 
-		if err := r.tfclient.UpdateSensitiveBeforeRun(workspace, specTFCVariables); err != nil {
-			r.reqLogger.Error(err, "Could not update Sensitive Varaibles before Run")
-			return reconcile.Result{}, err
-		}
-
 		if err := r.tfclient.CreateRun(instance, terraform); err != nil {
 			r.reqLogger.Error(err, "Could not run new Terraform configuration")
 			return reconcile.Result{}, err
