35a58a20b2be1572312fb4c4b497bedaa266d1b9
Stef Forrester <sforrester@hashicorp.com>
2020-05-19 15:43:12 -0700

    Make acceptance tests easier to run

--- operator/pkg/controller/workspace/tfc_org_test.go
+++ operator/pkg/controller/workspace/tfc_org_test.go
@@ -7,11 +7,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var (
-	testOrganization = "tf-operator"
-)
-
 func setupClient(t *testing.T, tfAddress string) (*TerraformCloudClient, error) {
+	testOrganization := os.Getenv("TF_ORG")
 	if os.Getenv("TF_ACC") == "" || os.Getenv("TF_CLI_CONFIG_FILE") == "" {
 		t.Skipf("this test requires Terraform Cloud and Enterprise access and credentials;" +
 			"set TF_ACC=1 and TF_CLI_CONFIG_FILE to run it")
