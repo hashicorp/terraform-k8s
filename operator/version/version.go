c1bf4b0f32abfce505adb28b55852bb351897a42
Kyriakos Oikonomakos <kyriakos@fastmail.com>
2020-09-15 11:09:02 +0100

    Upgrade operator-framework sdk to 0.18
    The latest version is 1.0.0 but upgrading from 0.18 to 0.19 requires
    major restructuring of the project. This represents a significant amount
    of work and will be done at a later time.

--- operator/version/version.go
+++
@@ -1,9 +0,0 @@
-package version
-
-var (
-	// Version of operator
-	Version = "0.1.4-alpha"
-
-	// TerraformVersion for workspace default
-	TerraformVersion = "0.12.25"
-)
--- version/version.go
+++ version/version.go
@@ -20,6 +20,9 @@ var (
 	// then it means that it is a final release. Otherwise, this is a pre-release
 	// such as "dev" (in development), "beta", "rc1", etc.
 	VersionPrerelease = "dev"
+
+	// TerraformVersion for workspace default
+	TerraformVersion = "0.12.25"
 )
 
 // GetHumanVersion composes the parts of the version in a way that's suitable
